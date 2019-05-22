e3c29636b0198036b3ea5345ee965fa6260d2146
Guru2921 <guru.softwaremaster@mail.ru>
2019-05-22 12:59:22 +1000

    initial

---
+++ build/precache-manifest.9239f0b94a68c2b05c4165f38aa4ad52.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "40dbe226ab43818eb12c",
+    "url": "/static/js/main.40dbe226.chunk.js"
+  },
+  {
+    "revision": "131b7f1e91a652791f08f5ccfe702645",
+    "url": "/static/media/line-awesome.131b7f1e.svg"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "/static/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/static/media/google.09aea0f5.svg"
+  },
+  {
+    "revision": "3d432ca2badb7e0130b379f9162b99b1",
+    "url": "/static/media/logo.3d432ca2.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "/static/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "/static/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "/static/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "c7c4adfda065e72b078a",
+    "url": "/static/js/2.c7c4adfd.chunk.js"
+  },
+  {
+    "revision": "c7c4adfda065e72b078a",
+    "url": "/static/css/2.476c285b.chunk.css"
+  },
+  {
+    "revision": "e939710cb876fb27d987561b36cbe050",
+    "url": "/index.html"
+  }
+];
